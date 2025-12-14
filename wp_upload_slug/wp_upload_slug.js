df4bd49dff44ec6f4f2db3f0ffdfe21f42fe8351
hamako0 <116480552+hamako0@users.noreply.github.com>
2025-12-15 00:08:50 +0900

    Add initial wp_upload_slug.js with error handling

---
+++ wp_upload_slug/wp_upload_slug.js
@@ -0,0 +1,15 @@
+try {
+  (function () {
+    (($) => {
+      window.$ = $;
+      let timestamp = new Date().getTime();
+      let dir = "https://hamako0.github.io/bookmarklet_hama/wp_upload_slug";
+      jQuery("body")
+        .append('<div id="root"></div>')
+        .append(`<link rel="stylesheet" href="${dir}/css/main.css?${timestamp}">`)
+        .append(`<script src="${dir}/js/main.js?${timestamp}"></script>`);
+    })(jQuery);
+  })();
+} catch (e) {
+  alert("ブックマクレッ・エラー\n" + e);
+}
