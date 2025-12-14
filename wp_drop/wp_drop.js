8a6db816155f0167a20c79ba068a5b53d5298cd1
hamako0 <116480552+hamako0@users.noreply.github.com>
2025-12-14 20:57:56 +0900

    Update directory URL in wp_drop.js

--- wp_drop/wp_drop.js
+++ wp_drop/wp_drop.js
@@ -3,7 +3,7 @@ try {
     (($) => {
       window.$ = $;
       let timestamp = new Date().getTime();
-      let dir = "https://hamako0.github.io/wp_tools/wp_drop";
+      let dir = "https://hamako0.github.io/bookmarklet_hama/wp_drop";
       jQuery("body")
         .append('<div id="root"></div>')
         .append(`<link rel="stylesheet" href="${dir}/css/main.css?${timestamp}">`)
